18d49cef687452c34b0e6267fb2af68f21c2c629
NicolasIannello <nicolas.iannello99@gmail.com>
2025-07-01 13:00:47 -0300

    -tracking

--- database/config.js
+++ database/config.js
@@ -6,7 +6,6 @@ const dbConnection = async() =>{
     try {
         await mongoose.connect(process.env.DB_CNN);
         console.log('Conectado a la base de datos '+new Date());
-        tracking();
     } catch (error) {
         console.log(error);
         throw new Error('Error al conectar con la base de datos')
