e45e1af583e222a634348951a0f8fce5ddfbfc1d
NicolasIannello <nicolas.iannello99@gmail.com>
2025-07-01 17:31:38 -0300

    jwt

---
+++ helpers/jwt.js
@@ -0,0 +1,36 @@
+const jwt =require('jsonwebtoken');
+
+const generarJWT=(id, mail, tipo)=>{
+    return new Promise((resolve,reject)=>{
+        const payload={ id, mail };
+
+        let secret, expired;
+        switch (tipo) {
+            case 'renew':
+                secret=process.env.JWT_SECRET_RENEW;
+                expired='72h';
+                break;
+            case 'validacion':
+                secret=process.env.JWT_SECRET_VALIDACION;
+                expired='2h';
+                break;
+            case 'pass':
+                secret=process.env.JWT_SECRET_PASS;
+                expired='2h';
+                break;
+        }
+
+        jwt.sign(payload, secret,{
+            expiresIn: expired
+        }, (err,token)=>{
+            if(err){
+                console.log(err);
+                reject(err)
+            }else{
+                resolve(token);
+            }
+        });
+    })
+}
+
+module.exports={generarJWT};
