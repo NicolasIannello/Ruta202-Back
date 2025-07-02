566e405e7965316ae3d9c9060e7458b80bbcde06
NicolasIannello <nicolas.iannello99@gmail.com>
2025-07-01 22:35:01 -0300

    validacion

---
+++ middlewares/validar-jwt.js
@@ -0,0 +1,42 @@
+const jwt = require("jsonwebtoken");
+
+const validarJWT=(req,res,next)=>{
+    const { token,tipo }=req.body
+    
+    if(!token){
+        return res.status(401).json({
+            ok:false,
+            msg:'no hay token'
+        });
+    }
+
+    try {        
+        let secret;
+        switch (tipo) {
+            case 1:
+                secret=process.env.JWT_SECRET_RENEW;
+                break;
+            case 2:
+                secret=process.env.JWT_SECRET_VALIDACION;                
+                break;
+            case 3:
+                secret=process.env.JWT_SECRET_PASS;
+                break;
+            default:
+                break;
+        }
+        const { id, mail }=jwt.verify(token,secret);
+        
+        req.id=id;
+        req.mail=mail;
+
+        next();
+    } catch (error) {
+        return res.status(401).json({
+            ok:false,
+            msg:'token mal'
+        });
+    }
+}
+
+module.exports={validarJWT};
