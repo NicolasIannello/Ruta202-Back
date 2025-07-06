fff05e385d84d9f9fe68263758102a4c1a21e7f9
NicolasIannello <nicolas.iannello99@gmail.com>
2025-07-06 17:14:01 -0300

    subirImagen folder / borrarImagen / getImg

--- helpers/imagenes.js
+++ helpers/imagenes.js
@@ -1,8 +1,10 @@
 const Imagen = require('../models/imagen');
 const { v4: uuidv4 }=require('uuid');
 const fs=require('fs');
+const path=require('path');
+const Usuario = require('../models/usuario');
 
-const subirImagen= async(imagen,UsuarioID,id,res)=>{
+const subirImagen= async(imagen,UsuarioID,id,res,tipo)=>{
     const img=imagen;
     const nombreCortado=img.name.split('.');
     const extensionArchivo=nombreCortado[nombreCortado.length-1];
@@ -11,10 +13,10 @@ const subirImagen= async(imagen,UsuarioID,id,res)=>{
     let datos;
     if(id==1){
         path= './files/vehiculo/'+nombreArchivo;
-        datos={ usuario: UsuarioID, img: nombreArchivo };
+        datos={ usuario: UsuarioID, img: nombreArchivo, tipo:tipo };
     }else{
         path= './files/carnet/'+nombreArchivo;
-        datos={ usuario: UsuarioID, img: nombreArchivo };    
+        datos={ usuario: UsuarioID, img: nombreArchivo, tipo:tipo };
     }
 
     img.mv(path, async (err)=>{
@@ -31,16 +33,46 @@ const subirImagen= async(imagen,UsuarioID,id,res)=>{
     })
 }
 
-// const borrarImagen= async(UsuarioID,folder)=>{
-//     const imagenDB= await Imagen.find({usuario:UsuarioID});
+const borrarImagen= async(UsuarioID,folder,tipo)=>{
+    const imagenDB= await Imagen.find({usuario:UsuarioID, tipo:tipo});
 
-//     for (let i = 0; i < imagenDB.length; i++) {
-//         let pathImg='./files/'+folder+'/'+imagenDB[i].img
-//         if(fs.existsSync(pathImg)) fs.unlinkSync(pathImg);
-//         await Imagen.findByIdAndDelete(imagenDB[i]._id);
-//     }
+    for (let i = 0; i < imagenDB.length; i++) {
+        let pathImg='./files/'+folder+'/'+imagenDB[i].img
+        if(fs.existsSync(pathImg)) fs.unlinkSync(pathImg);        
+        await Imagen.findByIdAndDelete(imagenDB[i]._id);
+    }
 
-//     return true;
-// }
+    return true;
+}
 
-module.exports={ subirImagen };
+const getImg= async(req,res = response) =>{
+    const { img }= req.query
+    const imgDB = await Imagen.findOne({img: img},{_id:0, __v:0})
+    
+    let usuarioDB
+    if(req.id){
+        usuarioDB = await Usuario.findById(req.id)
+        if(usuarioDB.UUID!=imgDB.usuario){
+            return res.status(500).json({
+                ok:false,
+                msg:'error en verificacion',
+            })
+        }
+    }
+
+    let pathImg;
+    if(imgDB.tipo=='vehiculo'){
+        pathImg=pathImg= path.join( __dirname, '../files/vehiculo/'+imgDB.img);
+    }else if(req.id){
+        pathImg=pathImg= path.join( __dirname, '../files/carnet/'+imgDB.img);
+    }
+
+    if(fs.existsSync(pathImg)){
+        res.sendFile(pathImg);
+    }else{
+        pathImg= path.join( __dirname, '../files/no-img.jpg');
+        res.sendFile(pathImg);
+    }
+};
+
+module.exports={ subirImagen, getImg, borrarImagen };
