3d6a6b1786371686788b30cbca2f46e78e1b767d
NicolasIannello <nicolas.iannello99@gmail.com>
2025-07-07 22:25:02 -0300

    token id

--- models/admin.js
+++ models/admin.js
@@ -3,10 +3,11 @@ const { Schema, model } = require('mongoose');
 const AdminSchema = Schema({
     Usuario: { type: String, required: true },
     Contrasena: { type: String, required: true },
+    TokenID: { type: Number, required: true }
 });
 
 AdminSchema.method('toJSON', function() {
-    const { __v, Contraseña, ...object } = this.toObject();
+    const { __v, Contrasena, ...object } = this.toObject();
     return object;
 });
 
