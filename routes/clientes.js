12a4084e12c3a291663edb9cd01a084f827bfceb
NicolasIannello <nicolas.iannello99@gmail.com>
2025-07-18 15:45:05 -0300

    getPedidos / getOfertas / borrarOferta / aceptarOferta

--- routes/clientes.js
+++ routes/clientes.js
@@ -2,7 +2,7 @@ const { Router }=require('express');
 const { check }=require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos');
 const { validarJWT } = require('../middlewares/validar-jwt');
-const { crearPedido } = require('../controllers/cliente');
+const { crearPedido, getPedidos, getOfertas, borrarOferta, aceptarOferta } = require('../controllers/cliente');
 
 const router=Router();
 
@@ -13,5 +13,36 @@ router.post('/crearPedido', [
 
     validarCampos,validarJWT
 ],crearPedido);
+
+router.post('/getPedidos', [
+    check('tipo','Campo obligatorio').not().isEmpty(),
+    check('token','Campo obligatorio').not().isEmpty(),
+
+    validarCampos,validarJWT
+],getPedidos);
+
+router.post('/getOfertas', [
+    check('tipo','Campo obligatorio').not().isEmpty(),
+    check('token','Campo obligatorio').not().isEmpty(),
+    check('_id','Campo obligatorio').not().isEmpty(),
+
+    validarCampos,validarJWT
+],getOfertas);
+
+router.post('/borrarOferta', [
+    check('tipo','Campo obligatorio').not().isEmpty(),
+    check('token','Campo obligatorio').not().isEmpty(),
+    check('_id','Campo obligatorio').not().isEmpty(),
+
+    validarCampos,validarJWT
+],borrarOferta);
+
+router.post('/aceptarOferta', [
+    check('tipo','Campo obligatorio').not().isEmpty(),
+    check('token','Campo obligatorio').not().isEmpty(),
+    check('_id','Campo obligatorio').not().isEmpty(),
+
+    validarCampos,validarJWT
+],aceptarOferta);
 
 module.exports=router;
