cc8eaedc3409f9823b40e110d8cbd5ff7477412c
NicolasIannello <nicolas.iannello99@gmail.com>
2025-10-13 17:17:12 -0300

    get orden

--- routes/imagenes.js
+++ routes/imagenes.js
@@ -1,7 +1,7 @@
 const { Router }=require('express');
 const expressFileUpload =require('express-fileupload');
 const { validarJWT, validarJWTAdmin } = require('../middlewares/validar-jwt');
-const { getImg } = require('../helpers/imagenes');
+const { getImg, getOrden } = require('../helpers/imagenes');
 
 const router=Router();
 
@@ -12,5 +12,7 @@ router.get('/img', getImg);
 router.get('/imgCarnet',validarJWT, getImg);
 
 router.get('/imgCarnetAdmin',validarJWTAdmin, getImg);
+
+router.get('/pdf',validarJWT, getOrden);
 
 module.exports=router;
